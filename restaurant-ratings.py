35c590bb3c0dbe5e75a7b79e59e2bec344f53fea
Hackbright Student <info@hackbrightacademy.com>
2016-10-03 16:38:18 -0700

    completes transforming scores.txt into a dictionary, then tuples list, and sorts that list. at pre-code review state

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,25 @@
 # your code goes here
+
+import sys
+
+file_name = sys.argv[1]
+
+def rate_restaurants(file_name):
+    """
+    """
+    with open(file_name) as restaurant_scores:
+
+        restaurants_dict = {}
+        for line in restaurant_scores:
+            print line
+            restaurant_info = line.strip().split(":")
+            print restaurant_info
+            key = restaurant_info[0]
+            value = restaurant_info[1]
+            restaurants_dict[key] = value
+            
+        tuple_list = restaurants_dict.items()
+        sorted_tuple_list = sorted(tuple_list)
+        return sorted_tuple_list
+
+print rate_restaurants(file_name)
