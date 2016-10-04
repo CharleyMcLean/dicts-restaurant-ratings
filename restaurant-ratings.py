9d2b4afb4e66c8d954ad823c6f468e11cb3b318f
Hackbright Student <info@hackbrightacademy.com>
2016-10-03 17:55:20 -0700

    Completed 'Giving the User Choices' section of Further Study.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -19,16 +19,29 @@ def rate_restaurants(file_name):
             value = restaurant_info[1]
             restaurants_dict[key] = value
 
-        new_restaurant = raw_input("Please enter a new restaurant: ")
-        new_rating = raw_input("Please enter that restaurant's rating: ")
-
-        restaurants_dict[new_restaurant] = new_rating
-
         tuple_list = restaurants_dict.items()
         sorted_tuple_list = sorted(tuple_list)
-        return sorted_tuple_list
 
-print rate_restaurants(file_name)
+        # if user_choice == "3":
+        #     break
+
+
+        while True:
+            user_choice = raw_input("To see restaurants and their ratings, press 1.  To add a new restaurant, press 2.  To quit, press 3.")
+            if user_choice == "1":
+                print sorted_tuple_list
+            elif user_choice == "2":
+                new_restaurant = raw_input("Please enter a new restaurant: ")
+                new_rating = raw_input("Please enter that restaurant's rating: ")
+                restaurants_dict[new_restaurant] = new_rating
+                tuple_list = restaurants_dict.items()
+                sorted_tuple_list = sorted(tuple_list)
+                print sorted_tuple_list
+            elif user_choice == "3":
+                break
+        
+
+rate_restaurants(file_name)
 
     
 
