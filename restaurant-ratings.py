f68899e1929c8152b485cea320599f9c76a9505f
Hackbright Student <info@hackbrightacademy.com>
2016-10-03 17:10:23 -0700

    After passing code review and added docstring.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -6,18 +6,19 @@ file_name = sys.argv[1]
 
 def rate_restaurants(file_name):
     """
+    Takes restaurants and ratings, and returns them alphabetized.
     """
     with open(file_name) as restaurant_scores:
 
         restaurants_dict = {}
         for line in restaurant_scores:
-            print line
+            # print line
             restaurant_info = line.strip().split(":")
-            print restaurant_info
+            # print restaurant_info
             key = restaurant_info[0]
             value = restaurant_info[1]
             restaurants_dict[key] = value
-            
+
         tuple_list = restaurants_dict.items()
         sorted_tuple_list = sorted(tuple_list)
         return sorted_tuple_list
